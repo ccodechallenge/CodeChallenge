b8641b9cb10acdd134262f05b1d51a5ae6aa81bd
NuriSEZGİN <nnurisezgin@gmail.com>
2020-01-11 16:20:03 +0300

    Added swagger as an API documentation to generate informations automatically.

--- src/main/java/com/challenge/demo/controller/DeviceControllerImp.java
+++ src/main/java/com/challenge/demo/controller/DeviceControllerImp.java
@@ -3,6 +3,7 @@ package com.challenge.demo.controller;
 import com.challenge.demo.service.DeviceService;
 import com.challenge.demo.service.entity.DeviceRequest;
 import com.challenge.demo.service.entity.DeviceResponse;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by nurisezgin on 11.01.2020.
  */
 @RestController
+@Api(tags = "Device API", description = "APIs for adding a new device to store or listing recorded devices in store.")
 public class DeviceControllerImp implements DeviceController {
 
     @Autowired
